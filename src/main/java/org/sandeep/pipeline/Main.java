23fe3e4f49c5af3e7a1f81025cc2adef226d48a2
sandeeprb <sandeeprb@ext.inditex.com>
2023-09-11 18:24:21 +0530

    initial commit
    Signed-off-by: sandeeprb <sandeeprb@ext.inditex.com>

---
+++ src/main/java/org/sandeep/pipeline/Main.java
@@ -0,0 +1,12 @@
+package org.sandeep.pipeline;
+
+public class Main {
+
+  /**
+   * Main class of the program
+   * @param args
+   */
+  public static void main(String[] args) {
+    System.out.println("This is a main class ...!");
+  }
+}
